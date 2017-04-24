8a7078d8b7fc821605586267f7c1c167bfdba469
PiNull <kwonsunbeom@gmail.com>
2017-04-25 04:30:16 +0900

    2017-04-25_04

--- brunch/brunch.py
+++ brunch/brunch.py
@@ -131,6 +131,6 @@ def job_function():
 
 sched = BlockingScheduler()
 
-# Schedules job_function to be run on the hour
+# Schedules job_function to be run on the hour (and more)
 sched.add_job(job_function, 'cron', minute='0-1,30-31')
 sched.start()
