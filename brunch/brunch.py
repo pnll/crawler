3131a5c5915308e0a5803933151469dc1fb59f63
PiNull <kwonsunbeom@gmail.com>
2017-04-27 05:00:08 +0900

    2017-04-27_05

--- brunch/brunch.py
+++ brunch/brunch.py
@@ -2,6 +2,7 @@
 import codecs
 import time
 import re
+import json
 from datetime import datetime
 from git import Repo
 from bs4 import BeautifulSoup
@@ -15,6 +16,8 @@ import logging.handlers
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 url = 'https://brunch.co.kr/'
+urlKeyword = 'https://api.brunch.co.kr/v1/top/keyword'
+
 path = './record/'
 #os.chdir(path)
 r = Repo('../')
@@ -38,80 +41,119 @@ streamHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
 logger.addHandler(streamHandler)
 
+
+def init_phantomjs_driver(*args, **kwargs):
+
+    headers = { 'User-Agent': 'brunch/1.5.5 Dalvik/2.1.0 (Linux; U; Android 7.0; SM-G935S Build/NRD90M)',
+        'BRUNCH-CLIENT-VERSION': '1.5.5',
+        'BRUNCH-CLIENT-OS': 'and',
+        'Host': 'api.brunch.co.kr',
+        'Connection': 'Keep-Alive',
+        'Accept-Encoding': 'gzip'
+    }
+
+    for key, value in headers.items():
+        webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.{}'.format(key)] = value
+
+    webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.settings.userAgent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+
+    driver =  webdriver.PhantomJS(*args, **kwargs)
+
+    return driver
+
+
 def job_function():
     now = datetime.now()
     nowDatetime = now.strftime('%Y-%m-%d_%H')
     print(nowDatetime)
     #print ( '%s-%s-%s' % ( now.year, now.month, now.day ) )
-    #date = '%s-%s-%s_%s:%s' % ( now.year, now.month, now.day, now.hour, now.min )
     #date = str(now)
     date = nowDatetime
-    #date_name = '%s-%s-%s_%s:%s' % ( now.year, now.month, now.day, now.hour, now.min )
+    #date_name = '%s-%s-%s_%s:%s' % ( now.year, now.month, now.day, now.hour, now.minute )
     date_name = nowDatetime
 
-    file_txt = path + date_name+'_tracking.txt'
+    file_txt = path + date_name+'_keyword.txt'
     file_html = path + date_name+'_recommended.html'
-    dcap = dict(DesiredCapabilities.PHANTOMJS)
-    dcap["phantomjs.page.settings.userAgent"] = (
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-        "(KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"
-    )
-    driver = webdriver.PhantomJS(desired_capabilities=dcap,executable_path=r'../../phantomjs-binaries/bin/phantomjs-2.1.1-linux-armhf')
-    driver.implicitly_wait(5)
-    driver.get(url)
-    #driver.find_element_by_id('mArticle')
+    file_json = path + date_name+'.json'
+    w=codecs.open(file_txt, encoding='utf-8', mode='a')
 
-    w=codecs.open(file_txt, encoding='utf-8', mode='w')
-    w2=codecs.open(file_html, encoding='utf-8', mode='w')
+    if now.minute%2:
+        w2=codecs.open(file_html, encoding='utf-8', mode='w')
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.userAgent"] = (
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"
+        )
+        driver = webdriver.PhantomJS(desired_capabilities=dcap,executable_path=r'../../phantomjs-binaries/bin/phantomjs-2.1.1-linux-armhf')
+        driver.get(url)
+        driver.implicitly_wait(5)
+        
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        #print (soup)
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    #print (soup)
+        result = soup.findAll("a", {"class":"keyword_item"})
+        print (str(len(result))) #24
+        #print (result)
 
-    result = soup.findAll("a", {"class":"keyword_item"})
-    print (str(len(result))) #24
-    #print (result)
+        for i in range(0, len(result)):
+            keyword = result[i].find('span',{'class':'keyword_item_txt'}).text
+            link = result[i]['href']
 
-    #time.sleep(1)
+            #print (date + ' > ' + keyword + ' : ' + link)
+            w.write(date+',\t' + keyword + ',\t' + link + ',\t\n')
+            logger.info(keyword + ' : ' + link)
+            #driver.find_element_by_class_name('keyword_item_txt')[i].click() #post_title has_image
 
-    for i in range(0, len(result)):
-        keyword = result[i].find('span',{'class':'keyword_item_txt'}).text
-        link = result[i]['href']
+        result = soup.find("div", {"class":"recommend_articles"}).findAll("a", {"class":"link_slide"})
+        print (str(len(result))) #30
+        #print (result)
+        w2.write('''<html>
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <title>RECOMMENDED ARTICLES</title>
+            <body><p>'''+str(now)+'</p>\n')
 
-        #print (date + ' > ' + keyword + ' : ' + link)
-        w.write(date+',\t' + keyword + ',\t' + link + ',\t\n')
-        logger.info(keyword + ' : ' + link)
-        #driver.find_element_by_class_name('keyword_item_txt')[i].click() #post_title has_image
+        for i in range(0, len(result)):
+            org = result[i].find('div',{'class':'img_articles'})
+            if org is None:
+                img = ''
+            else:
+                img = str(org.find('img'))
+                #img_src = result[i].find('div',{'class':'img_articles'}).find('img')['src']
+                img = re.sub('src="//', 'src="http://', img)
 
-    result = soup.find("div", {"class":"recommend_articles"}).findAll("a", {"class":"link_slide"})
-    print (str(len(result))) #30
-    #print (result)
-    w2.write('''<html>
-        <head>
-            <meta charset="UTF-8">
-        </head>
-        <title>RECOMMENDED ARTICLES</title>
-        <body><p>'''+str(now)+'</p>\n')
+            title = result[i].find('strong',{'class':'tit_subject'}).text
+            body = result[i].find('p',{'class':'desc_subject'}).text
+            author = result[i].find('span',{'class':'info_by'}).text
 
-    driver.implicitly_wait(3)
-    for i in range(0, len(result)):
-        img = str(result[i].find('div',{'class':'img_articles'}).find('img'))
-        img_src = result[i].find('div',{'class':'img_articles'}).find('img')['src']
-        #f = open("./img/" + str(i) + ".png", "wb")
-        img = re.sub('src="//', 'src="http://', img)
-        #img_req = urllib.request.Request(img)
-        #f.write(urllib.request.urlopen(img_req).read())
-        #f.close()
+            #print (img + '<br><h3>' + title + '</h3><br>' + body + '<p>\n')
+            w2.write('<p>' + img + '<br><h3>' + title + '</h3><h6>' + author + '</h6>' + body + '</p>\n')
+            logger.debug(title + ' ' + author.replace(u'\xa0', ' ') + ' : ' + body.replace(u'\u200b', ' '))
 
-        title = result[i].find('strong',{'class':'tit_subject'}).text
-        body = result[i].find('p',{'class':'desc_subject'}).text
-        author = result[i].find('span',{'class':'info_by'}).text
+        w2.write('\n</body><html>')
+        w2.close()
 
-        #print (img + '<br><h3>' + title + '</h3><br>' + body + '<p>\n')
-        w2.write('<p>' + img + '<br><h3>' + title + '</h3><h6>' + author + '</h6>' + body + '</p>\n')
-        logger.debug(title + ' ' + author.replace(u'\xa0', ' ') + ' : ' + body.replace(u'\u200b', ' '))
+    else:
+        w3=codecs.open(file_json, encoding='utf-8', mode='w')
+        driver = init_phantomjs_driver(executable_path=r'../../phantomjs-binaries/bin/phantomjs-2.1.1-linux-armhf')
+        driver.get(urlKeyword)
+        driver.implicitly_wait(5)
+        
+        pre = driver.find_element_by_tag_name("pre").text
+        data = json.loads(pre)
+        #print (data)
+        w3.write(pre)
 
-    w2.write('\n</body><html>')
+        for l in data['data']['list']:
+            if l['type'] == "KEYWORD_ARTICLE_AUTO":
+                title = l['contents']['keywordTitle']
+                groupNo = str(int(l['contents']['groupNo']))
+                w.write(date+',\t' + groupNo + ',\t' + title + ',\t\n')
+                logger.info(groupNo + ' : ' + title)
+            #if 'keywordTitle' in l['contents']:
+        w3.close()
 
     time.sleep(1)
 
@@ -119,9 +161,8 @@ def job_function():
     #webbrowser.open(file_html) # see results
 
     w.close()
-    w2.close()
     driver.quit()
-    logger.info('Completed writing files')
+    logger.debug('Completed writing files')
 
     time.sleep(5)
 
@@ -129,7 +170,8 @@ def job_function():
     r.git.add('.')
     r.git.commit(m=date_name)
     r.git.push()
-    logger.info('Completed to commit and push on GitHub')
+    logger.debug('Completed to commit and push on GitHub')
+
 
 sched = BlockingScheduler()
 
